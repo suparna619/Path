aca450ccf2fa32091d38161839690073200abd21
Suparna <suparnad@thoughtworks.com>
2015-03-07 20:16:41 +0530

    Main modified

--- src/com/paths/PathsMain.java
+++ src/com/paths/PathsMain.java
@@ -1,19 +1,35 @@
 package com.paths;
 
+import static java.lang.System.exit;
+
 public class PathsMain {
 
     public static void main(String[] args) {
-        String sourceStaton = args[0];
-        String destinationStation = args[1];
-        Paths newPath = new Paths();
-        String path;
-        if(!newPath.isStationPresent(sourceStaton)){
-            System.out.println("No city named '" + sourceStaton + "' in database");
+        String option = args[0];
+        String file = args[1];
+        String sourceStation = args[2].toLowerCase();
+        String destinationStation = args[3].toLowerCase();
+        ReadFile fileReader = new ReadFile();
+        String content = fileReader.readFile(file);
+        Paths path = new Paths(content);
+
+        if(!option.equals("-f")) {
+            System.out.println("Wrong Option '"+option+"'");
+            exit(0);
         }
-        if(!newPath.isStationPresent(destinationStation)){
-            System.out.println("No city named '" + destinationStation + "' in database");
+        if(!path.isStationPresent(sourceStation)){
+            System.out.println("No city named '" + sourceStation.toUpperCase() + "' in database");
+            exit(0);
+        }
+        if(sourceStation.equals(destinationStation)){
+            System.out.println("Source and Destination are equal");
+            exit(0);
+        }
+        if(!path.isStationPresent(destinationStation)){
+            System.out.println("No city named '" + destinationStation.toUpperCase() + "' in database");
+            exit(0);
         }
         else
-            System.out.println(newPath.printPath(sourceStaton,destinationStation));
+            System.out.println(path.printPath(sourceStation,destinationStation));
     }
 }
