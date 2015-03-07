d81e829306116cc31fd9b29f07dddc10199db0a5
Suparna <suparnad@thoughtworks.com>
2015-03-07 22:31:34 +0530

    Version - 4 modified

--- src/com/paths/ReadFile.java
+++ src/com/paths/ReadFile.java
@@ -4,10 +4,16 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static java.lang.System.exit;
+
 public class ReadFile{
     public String readFile(String fileName) {
         String text = null;
         File file = new File(fileName);
+        if(!file.exists()){
+            System.out.println("No database named "+fileName+" found");
+            exit(0);
+        }
         try {
             FileReader reader = new FileReader(file);
             char[] chars = new char[(int) file.length()];
